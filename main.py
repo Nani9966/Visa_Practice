a10159b06f1fae120b0847b7bb0c17ae95f92cac
chinna reddy <chinnareddy2106@gmail.com>
2023-04-18 00:29:06 +0530

    update with setup code ofor version setup

---
+++ main.py
@@ -0,0 +1,8 @@
+a = 0
+b= 1
+sum =0
+for i in range (0 , 10):
+    a =b
+    b=sum
+    sum =a+b
+    print(sum,end = " ")
---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from typing import List
+
+                                         
+                 
+                       
+                                                                       
+                     
+                                          
+
+
+project_name= "ML_Project"
+VERSION = "0.0.1"
+AUTHOR = "chinna reddy"
+DESCRIPTION = "This is our Machine learning pipline buliding session "
+HYPHEN_E_DOT='-e .'
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+def get_requirements_list()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list=requirement_file.readlines()
+        requirement_list=[requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
+
+
+
+setup(
+name=project_name,   
+version=VERSION,   
+description=DESCRIPTION,
+author=AUTHOR,
+packages=find_packages(),
+install_requires=get_requirements_list()
+)
