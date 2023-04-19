9d99f3775fa81d21c7d9648e26256efa924d6b4f
chinna reddy <chinnareddy2106@gmail.com>
2023-04-20 00:08:45 +0530

    data_ingestion update

---
+++ visa/utils/utils.py
@@ -0,0 +1,12 @@
+import os,sys
+
+from visa.exception import CustomException
+
+
+def read_yaml_file(file_path:str)-> str:
+
+    try:
+        with open(file_path,'rb') as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except Exception as e:
+        raise CustomException(e,sys) as e
